dcf67e9bdd5f8e6ee658ce4378a3d2c212031113
nekocatchan <f@oken.fun>
2025-01-03 03:47:46 +0900

    ♻️ リファクタリング: interpret関数を分割し、単項および二項演算子の処理を明確化

--- interpreter.js
+++ interpreter.js
@@ -1,48 +1,46 @@
 const interpret = (ast) => {
   switch (ast.type) {
-    case "Number": {
+    case "Number":
       return ast.value;
-    }
 
-    case "UnaryExpression": {
-      const argument = interpret(ast.argument);
-
-      switch (ast.operator) {
-        case "-": {
-          return -argument;
-        }
-        default: {
-          throw new Error(`Unknown operator: ${ast.operator}`);
-        }
-      }
-    }
+    case "UnaryExpression":
+      return interpretUnaryExpression(ast);
 
     case "BinaryExpression": {
-      const left = interpret(ast.left);
-      const right = interpret(ast.right);
-
-      switch (ast.operator) {
-        case "+": {
-          return left + right;
-        }
-        case "-": {
-          return left - right;
-        }
-        case "*": {
-          return left * right;
-        }
-        case "/": {
-          return left / right;
-        }
-        default: {
-          throw new Error(`Unknown operator: ${ast.operator}`);
-        }
-      }
+      return interpretBinaryExpression(ast);
     }
 
     default: {
       throw new Error(`Unknown AST node type: ${ast.type}`);
     }
+  }
+};
+
+const interpretUnaryExpression = (ast) => {
+  const argument = interpret(ast.argument);
+
+  if (ast.operator === "-") {
+    return -argument;
+  } else {
+    throw new Error(`Unknown operator: ${ast.operator}`);
+  }
+};
+
+const interpretBinaryExpression = (ast) => {
+  const left = interpret(ast.left);
+  const right = interpret(ast.right);
+
+  switch (ast.operator) {
+    case "+":
+      return left + right;
+    case "-":
+      return left - right;
+    case "*":
+      return left * right;
+    case "/":
+      return left / right;
+    default:
+      throw new Error(`Unknown operator: ${ast.operator}`);
   }
 };
 
